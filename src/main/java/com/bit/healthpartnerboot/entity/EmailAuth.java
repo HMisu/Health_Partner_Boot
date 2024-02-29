1e59a677b910106ca4c85fc9053b4308421e9150
misu <jagalji@daum.net>
2024-02-29 16:56:27 +0900

    feat(entity): adding member

---
+++ src/main/java/com/bit/healthpartnerboot/entity/EmailAuth.java
@@ -0,0 +1,40 @@
+package com.bit.healthpartnerboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "TB_EMAIL_AUTH")
+public class EmailAuth {
+    private static final Long MAX_EXPIRE_TIME = 5L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email_auth_seq")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_seq")
+    private Member member;
+
+    private AuthType authType;
+
+    private String authToken;
+
+    private Boolean expired;
+
+    private LocalDateTime expireDate;
+
+    public void useToken() {
+        this.expired = true;
+    }
+}
