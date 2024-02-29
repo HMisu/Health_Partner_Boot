20cc152e4ace1aa4d3197ffcaf571decdc35839e
misu <jagalji@daum.net>
2024-02-29 14:35:44 +0900

    feat(config): adding configuration files... chore : build.gradle setting

--- src/main/java/com/bit/healthpartnerboot/api/ApiExplorer.java
+++ src/main/java/com/bit/healthpartnerboot/api/ApiExplorer.java
@@ -5,15 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class ApiExplorer {
     public void getApi() {
         try {
-            /*URL*/
-            String urlBuilder = "http://apis.data.go.kr/1390802/AgriFood/MzenFoodNutri/getKoreanFoodIdntList" + "?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=8ZcskwShapZjaTsOEAyi%2FH6j66Q98M0c2q%2BJ6DHxWAqNqLw5KPO1dYEp8XVIYJ8YHXaRq4VDMjcCWK6N0Qn8lw%3D%3D" + /*Service Key*/
-                    "&" + URLEncoder.encode("food_Code", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("D061001", StandardCharsets.UTF_8); /*음식 코드 식별ID 값 * 식단관리(메뉴젠) 음식 정보 API 참조 (https://www.data.go.kr/data/15077804/openapi.do)*/
+            String urlBuilder = "http://apis.data.go.kr/1390802/AgriFood/FdImage/getKoreanFoodFdImageList?serviceKey=8ZcskwShapZjaTsOEAyi%2FH6j66Q98M0c2q%2BJ6DHxWAqNqLw5KPO1dYEp8XVIYJ8YHXaRq4VDMjcCWK6N0Qn8lw%3D%3D&service_Type=json&Page_No=1&Page_Size=20";
             URL url = new URL(urlBuilder);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -21,9 +17,10 @@ public class ApiExplorer {
             System.out.println("Response code: " + conn.getResponseCode());
             BufferedReader rd;
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             } else {
-                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+                System.out.println("Response code: " + conn.getResponseCode());
             }
             StringBuilder sb = new StringBuilder();
             String line;
